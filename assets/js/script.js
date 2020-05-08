ce37ad9ff25e60f98b27f595e7f16fa59647054d
Manuel Salinas <msalinasdiaz@gmail.com>
2020-05-08 19:13:21 -0400

    prueba de commit

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+/*SMOOTH SCROLL*/
+
+$(function(){
+	$("a").click(function(event){
+		if (this.hash !== "") {
+			event.preventDefault();
+			
+			var gato = this.hash;
+			
+			$("html, body").animate({ scrollTop: $(gato).offset() .top}, 800, function (){
+				window.location.hash = gato;
+				
+			});
+		}
+	});
+});
+
+
+
+
+/*TOOLTIP*/
+$(document).ready(function(){
+	$('[data-toggle="tooltip"]').tooltip();
+});
+
+
+
+$('#myModal').on('shown.bs.modal', function () {
+	$('#myInput').trigger('focus')
+})
